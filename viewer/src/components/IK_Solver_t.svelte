bc0137e6a7743239a8637a5fc29c6b71a4d95d7e
CelestineOverflow <77799581+CelestineOverflow@users.noreply.github.com>
2024-05-06 00:32:04 +0200

    fix ik rig

--- viewer/src/components/IK_Solver_t.svelte
+++ viewer/src/components/IK_Solver_t.svelte
@@ -15,14 +15,14 @@
     let defaultBoneDirection: THREE.Vector3;
     let defaultBoneLength: number;
     let target: THREE.Vector3;
-    let upperArmBone : any;
-    let forearmBone : any;
-    let handBone : any;
-    let target_handL : any;
-    let transformControls : TransformControls;
-    let orbitControls : OrbitControls;
-    let solver : CCDIKSolver;
-    let cube : THREE.Mesh;
+    let upperArmBone: any;
+    let forearmBone: any;
+    let handBone: any;
+    let target_handL: any;
+    let transformControls: TransformControls;
+    let orbitControls: OrbitControls;
+    let solver: CCDIKSolver;
+    let cube: THREE.Mesh;
 
     onMount(() => {
         init();
@@ -34,7 +34,6 @@
     let availableBones: string[] = [];
     let model: any;
 
-
     function findBone(root, name) {
         let target = null;
         root.traverse((node) => {
@@ -44,108 +43,112 @@
         });
         return target;
     }
-    let skeleton : any;
-    
+    let skeleton: any;
 
-function add_rigged_model() {
-    const loader = new GLTFLoader();
-    loader.load("test_ik3.glb", function (gltf) {
-        model = gltf.scene;
-        let skinnedMesh;
-        model.traverse((node) => {
-            if (node.name === "lowman_shoes") {
-                node.traverse((child) => {
-                    if (child.type === "SkinnedMesh") {
-                        skinnedMesh = child;
+    function add_rigged_model() {
+        const loader = new GLTFLoader();
+        loader.load("test_ik3.glb", function (gltf) {
+            model = gltf.scene;
+            let skinnedMesh;
+            model.traverse((node) => {
+                if (node.name === "lowman_shoes") {
+                    node.traverse((child) => {
+                        if (child.type === "SkinnedMesh") {
+                            skinnedMesh = child;
+                        }
+                    });
+                }
+            });
+
+            if (skinnedMesh) {
+                console.log("SkinnedMesh found", skinnedMesh);
+                // model.scale.set(20, 20, 20);
+                console.log(skinnedMesh.skeleton.bones);
+                for (let i = 0; i < skinnedMesh.skeleton.bones.length; i++) {
+                    if (skinnedMesh.skeleton.bones[i].name === "upper_armL") {
+                        upperArmBone = skinnedMesh.skeleton.bones[i];
+                    } else if (
+                        skinnedMesh.skeleton.bones[i].name === "forearmL"
+                    ) {
+                        forearmBone = skinnedMesh.skeleton.bones[i];
+                    } else if (skinnedMesh.skeleton.bones[i].name === "handL") {
+                        handBone = skinnedMesh.skeleton.bones[i];
+                    } else if (
+                        skinnedMesh.skeleton.bones[i].name ===
+                        "index_target_handL"
+                    ) {
+                        target_handL = skinnedMesh.skeleton.bones[i];
                     }
-                });
+                }
+                if (upperArmBone && forearmBone && handBone) {
+                    console.log(
+                        "Bones found:",
+                        upperArmBone,
+                        forearmBone,
+                        handBone,
+                    );
+                    let index_upperArm =
+                        skinnedMesh.skeleton.bones.indexOf(upperArmBone);
+                    let index_forearm =
+                        skinnedMesh.skeleton.bones.indexOf(forearmBone);
+                    let index_hand =
+                        skinnedMesh.skeleton.bones.indexOf(handBone);
+                    let index_target_handL =
+                        skinnedMesh.skeleton.bones.indexOf(target_handL);
+                    console.log(
+                        "Indices:",
+                        index_upperArm,
+                        index_forearm,
+                        index_hand,
+                        index_target_handL,
+                    );
+
+                    // Define IK Chains
+
+                    const iks = [
+                        {
+                            target: index_target_handL, // "target"
+                            effector: index_hand, // "bone3"
+                            links: [
+                                {
+                                    index: index_forearm,
+                                    rotationMin: new THREE.Vector3(
+                                        1.2,
+                                        -1.8,
+                                        -0.4,
+                                    ),
+                                    rotationMax: new THREE.Vector3(
+                                        1.7,
+                                        -1.1,
+                                        0.3,
+                                    ),
+                                },
+                                { index: index_upperArm,
+                                    
+                                },
+                            ], // "bone2", "bone1", "bone0"
+                        },
+                    ];
+
+                    solver = new CCDIKSolver(skinnedMesh, iks);
+                } else {
+                    console.error("Error: One or more bones not found.");
+                }
+                //add reference to the target
+                // let axis_helper = new THREE.AxesHelper(10);
+                // axis_helper.position.copy(target_handL.position);
+                // scene.add(axis_helper);
+
+                // Visualize skeleton
+                skeleton = new THREE.SkeletonHelper(model);
+                skeleton.visible = true;
+                scene.add(skeleton);
+                scene.add(model);
+            } else {
+                console.error("SkinnedMesh not found.");
             }
         });
-
-        if (skinnedMesh) {
-            console.log("SkinnedMesh found", skinnedMesh);
-            model.scale.set(20, 20, 20);
-            console.log(skinnedMesh.skeleton.bones);
-            for (let i = 0; i < skinnedMesh.skeleton.bones.length; i++) {
-                if (skinnedMesh.skeleton.bones[i].name === "upper_armL") {
-                    upperArmBone = skinnedMesh.skeleton.bones[i];
-                } else if (skinnedMesh.skeleton.bones[i].name === "forearmL") {
-                    forearmBone = skinnedMesh.skeleton.bones[i];
-                } else if (skinnedMesh.skeleton.bones[i].name === "handL") {
-                    handBone = skinnedMesh.skeleton.bones[i];
-                } else if (skinnedMesh.skeleton.bones[i].name === "index_target_handL") {
-                    target_handL = skinnedMesh.skeleton.bones[i];
-                }
-            }
-            if (upperArmBone && forearmBone && handBone) {
-                console.log("Bones found:", upperArmBone, forearmBone, handBone);
-                let index_upperArm = skinnedMesh.skeleton.bones.indexOf(upperArmBone);
-                let index_forearm = skinnedMesh.skeleton.bones.indexOf(forearmBone);
-                let index_hand = skinnedMesh.skeleton.bones.indexOf(handBone);
-                let index_target_handL = skinnedMesh.skeleton.bones.indexOf(target_handL);
-                console.log("Indices:", index_upperArm, index_forearm, index_hand, index_target_handL);
-                
-                // Define IK Chains
-
-                const iks = [
-                {
-                target: index_target_handL, // "target"
-                effector: index_hand, // "bone3"
-                links: [ { index: index_forearm }, { index: index_upperArm } ] // "bone2", "bone1", "bone0"
-                }
-                ];
-
-                // Initialize CCDIKSolver with the SkinnedMesh
-                solver = new CCDIKSolver(skinnedMesh, iks);
-            } else {
-                console.error("Error: One or more bones not found.");
-            }
-
-            // Visualize skeleton
-            skeleton = new THREE.SkeletonHelper(model);
-            skeleton.visible = true;
-            scene.add(skeleton);
-            scene.add(model);
-        } else {
-            console.error("SkinnedMesh not found.");
-        }
-    });
-}
-    
-    // function add_rigged_model() {
-    //     const loader = new GLTFLoader();
-    //     loader.load("test_ik3.glb", function (gltf) {
-    //         model = gltf.scene;
-    //         let skinedMesh ;
-    //         model.traverse((node) => {
-    //             // console.log(node.name);
-    //             if (node.name === "lowman_shoes") {
-    //                 //print the type of the node
-    //                 console.log(node.type);
-    //                 node.traverse((child) => {
-    //                     //print the name of the child node
-    //                     console.log(child.type);
-    //                     if (child.type === "SkinnedMesh") {
-    //                         skinedMesh = child;
-    //                     }
-    //                 });
-    //             }
-    //         });
-    //         if (skinedMesh) {
-    //             console.log("skinedMesh found");
-    //         }
-    //         model.scale.set(20, 20, 20);
-    //         upperArmBone = findBone(model, "upper_armL");
-    //         forearmBone = findBone(model, "forearmL");
-    //         handBone = findBone(model, "handL");
-    //         skeleton = new THREE.SkeletonHelper( model );
-    //         skeleton.visible = true;
-    //         scene.add( skeleton );
-    //         scene.add(model);
-    //     });
-        
-    // }
-
+    }
     function init() {
         renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
         renderer.setSize(window.innerWidth, window.innerHeight);
@@ -176,21 +179,27 @@ function add_rigged_model() {
         scene.add(light);
         add_rigged_model();
         //orbitControls = new OrbitControls( camera, renderer.domElement );
-        transformControls = new TransformControls( camera, renderer.domElement );
+        transformControls = new TransformControls(camera, renderer.domElement);
         transformControls.size = 0.75;
-        transformControls.space = 'world';
-        cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+        transformControls.space = "world";
+        cube = new THREE.Mesh(
+            new THREE.BoxGeometry(0.1, 0.1, 0.1),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+        );
         cube.position.set(3, 4, 0);
         scene.add(cube);
-        transformControls.attach( cube );
-        scene.add( transformControls );
-        transformControls.addEventListener( 'mouseDown', () => controls.enabled = false );
-        transformControls.addEventListener( 'mouseUp', () => controls.enabled = true );
+        transformControls.attach(cube);
+        scene.add(transformControls);
+        transformControls.addEventListener(
+            "mouseDown",
+            () => (controls.enabled = false),
+        );
+        transformControls.addEventListener(
+            "mouseUp",
+            () => (controls.enabled = true),
+        );
         render();
     }
-
-
-    
 
     function resize() {
         renderer.setSize(window.innerWidth, window.innerHeight);
